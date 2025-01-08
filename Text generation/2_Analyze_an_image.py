add09b26d41216a8ce708d296c248af5d8d15b4b
엔카코 <ncaco97@gmail.com>
2025-01-08 20:42:49 +0900

    Create 2_Analyze_an_image.py

---
+++ Text generation/2_Analyze_an_image.py
@@ -0,0 +1,25 @@
+# Describe the contents of an image
+
+from openai import OpenAI
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                    }
+                },
+            ],
+        }
+    ],
+)
+
+print(completion.choices[0].message)
