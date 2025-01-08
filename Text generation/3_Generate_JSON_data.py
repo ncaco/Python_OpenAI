1da6cc591e25db20cab00227249c95c1c32092a8
엔카코 <ncaco97@gmail.com>
2025-01-08 20:43:22 +0900

    Create 3_Generate_JSON_data.py

---
+++ Text generation/3_Generate_JSON_data.py
@@ -0,0 +1,36 @@
+#Generate JSON data based on a JSON Schema
+
+from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o-2024-08-06",
+    messages=[
+        {
+            "role": "developer", 
+            "content": "You extract email addresses into JSON data."
+        },
+        {
+            "role": "user", 
+            "content": "Feeling stuck? Send a message to help@mycompany.com."
+        }
+    ],
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "email_schema",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "email": {
+                        "description": "The email address that appears in the input",
+                        "type": "string"
+                    },
+                    "additionalProperties": False
+                }
+            }
+        }
+    }
+)
+
+print(response.choices[0].message.content);
